ace1914d57d1acba607442cfa6db84bf9948327d
Wesley B. Howard <bryce9643@gmail.com>
2021-02-07 02:35:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function add(a, b) {
   var result = (a + b)
   return (`${result}`)
 }
+
+function subtract(a, b) {
+  var result2 = (a - b)
+  return (`${result2}`)
+}
+
+function multiply(a, b) {
+  var result3 = (a * b)
+  return (`${result3}`)
+}
+
+function divide(a, b) {
+  var result4 = (a / b)
+  return (`${result4}`)
+}
